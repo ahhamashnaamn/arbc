2ad4b3c50b5e041e7c8c58ea08aeac723eb9e447
sajadaltaie <sajadaltaie2010@gmail.com>
2015-12-24 20:54:11 +0300

    رسائل ترحيب

--- plugins/service_entergroup.lua
+++ plugins/service_entergroup.lua
@@ -58,9 +58,9 @@ local function description_rules(msg, nama)
          rules = data[tostring(msg.to.id)]["rules"]
          rules = "\nRules :\n"..rules.."\n"
       end
-      local sambutan = "You are in group '"..string.gsub(msg.to.print_name, "_", " ").."'\n"
+      local sambutan = "انت في كروب '"..string.gsub(msg.to.print_name, "_", " ").."'\n"
       local text = sambutan..about..rules.."\n"
-      local text = text.."Please welcome "..nama
+      local text = text.."مرحبا بك "..nama
       local receiver = get_receiver(msg)
       send_large_msg(receiver, text, ok_cb, false)
    end
@@ -89,7 +89,7 @@ local function run(msg, matches)
       description_rules(msg, nama)
    elseif matches[1] == "chat_del_user" then
        local bye_name = msg.action.user.first_name
-       return 'Bye '..bye_name..'!'
+       return 'وداعا '..bye_name..'!'
    end
 end
 
