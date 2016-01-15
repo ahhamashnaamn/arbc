383edd4ad37ea30e0e33b3d92ec43fb8db0463bc
Shahab <safaeishahab@yahoo.com>
2016-01-15 19:59:10 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -220,7 +220,7 @@ function create_config( )
       "version",
       "link",
       "moderation"},
-    sudo_users = {our_id},
+    sudo_users = {119989724},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
